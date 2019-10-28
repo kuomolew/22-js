9280430dd80221fe14af86168cbd218c8d766a48
kuomolew <kuomolew@gmail.com>
2019-10-28 20:40:07 +0200

    lesson

---
+++ js/xx-lesson.js
@@ -0,0 +1,27 @@
+console.log('Hello world');
+
+// for (i=0; i<2; i++) {
+//     var a = i;
+//     let b = i;
+//     console.log("inside a = ", a);
+//     console.log("inside b = ", b);
+// }
+
+//console.log("outside a = ", a);
+//console.log("inside b = ", b);
+
+// console.log("  \"  a");
+
+//let a = NaN;
+
+let x = 12;
+let y = '1';
+
+let r = 4;
+
+let aa = x + r + y;
+
+console.log(aa);
+
+let bb = x + y + r;
+console.log(bb);
