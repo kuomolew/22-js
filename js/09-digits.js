76bc700839f4dc70ce9d30b67688b25eec0f8799
kuomolew <kuomolew@gmail.com>
2019-11-01 22:23:31 +0200

    done

---
+++ js/09-digits.js
@@ -0,0 +1,17 @@
+document.getElementById('counter').insertAdjacentHTML("afterend", '<h1>Magic... <span id="newNumber"></span></h1>');
+
+let calculate = function() {
+    let number = document.getElementById('number').value;
+    if(number > 99 && number <= 999) {
+        let newNumber = '';
+        while (number >= 1) {
+            newNumber = newNumber + number % 10;
+            number = Math.floor(number / 10);
+        }
+        
+        
+        document.getElementById('newNumber').innerText = newNumber;
+    } else {
+        document.getElementById('newNumber').innerText = '3 digits. 3! Is it so hard?';
+    }
+}
---
+++ js/10-deposit.js
@@ -0,0 +1,14 @@
+document.getElementById('counter').insertAdjacentHTML("afterend", '<h1>You will earn <span id="earn"></span>$</h1>');
+
+let calculate = function() {
+    let sum = document.getElementById('sum').value;
+    if(sum > 0) {
+        const rate = 5;
+        let earn = (sum * (5*2/12) /100).toFixed(2);
+        
+        
+        document.getElementById('earn').innerText = earn;
+    } else {
+        document.getElementById('earn').innerText = 'We need more from you';
+    }
+}
